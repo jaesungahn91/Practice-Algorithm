5ca8c55fd5dbe6214b28743dd744392715c74bdc
ahnjs <ahnjs@eseict.com>
2021-06-01 15:06:09 +0900

    [BOJ, 1193] 분수찾기
    https://www.acmicpc.net/problem/1193

---
+++ src/baekjoon/BOJ_1193.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_1193 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+        int sum = 0;
+        int maxNum = 1;
+        int bunmo = 1;
+        int bunja = 1;
+        for (int i = 0; sum < X; i++) {
+
+            sum += i;
+            maxNum = i;
+        }
+
+        if(maxNum % 2 == 0){
+            bunja = X - (sum - maxNum);
+            bunmo = sum - X + 1;
+        } else {
+            bunmo = X - (sum - maxNum);
+            bunja = sum - X + 1;
+        }
+        System.out.println(bunja+"/"+bunmo);
+    }
+}
