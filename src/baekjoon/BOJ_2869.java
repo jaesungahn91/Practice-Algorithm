bd1947120e587878f60693ecd65aa9a155189789
jaesungahn91 <jaesungahn91@gmail.com>
2021-06-03 08:37:37 +0900

    [BOJ, 2869] 달팽이는 올라가고 싶다
    https://www.acmicpc.net/problem/2869

---
+++ src/baekjoon/BOJ_2869.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+// 달팽이는 올라가고 싶다
+public class BOJ_2869 {
+    public static void main(String[] args) throws IOException {
+        // 높이 V
+        // 낮 A 올라감. 밤 B 미끄러짐
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int day = 1;
+
+        day = ((V - B) / (A - B));
+
+        if (((V - B) % (A - B)) != 0) day++;
+
+        System.out.println(day);
+    }
+}
