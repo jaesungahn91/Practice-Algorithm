047f5c219337e61ed3ee2168a740e0d3689053ef
jaesungahn91 <jaesungahn91@gmail.com>
2021-06-07 23:10:49 +0900

    [BOJ, 10250] ACM 호텔
    https://www.acmicpc.net/problem/10250

---
+++ src/baekjoon/BOJ_10250.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// ACM 호텔
+public class BOJ_10250 {
+
+    public static void main(String[] args) throws IOException {
+        // 1 ≤ H, W ≤ 99, 1 ≤ N ≤ H × W
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        
+    }
+}
