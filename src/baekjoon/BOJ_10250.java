c103e0ca06b2e0c2c63feefca5a21812f6369c76
jaesungahn91 <jaesungahn91@gmail.com>
2021-06-14 08:34:57 +0900

    [BOJ, 10250] ACM 호텔
    https://www.acmicpc.net/problem/10250

--- src/baekjoon/BOJ_10250.java
+++ src/baekjoon/BOJ_10250.java
@@ -12,6 +12,28 @@ public class BOJ_10250 {
         // 1 ≤ H, W ≤ 99, 1 ≤ N ≤ H × W
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int X, Y;
+
+            // 매층 첫번쨰 방을 우선으로한다.
+            // 거리가 같을 경우 아래층 방을 선호.
+            // 숙박하고있는 방은 제외 해야한다.
+            // H W N 순으로 받는다.
+            if (N % H == 0) {
+                Y = H * 100;
+                X = (N / H);
+            } else {
+                Y = (N % H) * 100;
+                X = (N / H) + 1;
+            }
+            System.out.println(Y + X);
+        }
     }
 }
