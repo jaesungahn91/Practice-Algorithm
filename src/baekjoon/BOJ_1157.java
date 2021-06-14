0ebd52b3fdee8d7f2f074b57e4935d7277f5c37f
jaesungahn91 <jaesungahn91@gmail.com>
2021-06-15 08:34:44 +0900

    [BOJ, 1157] 단어 공부
    https://www.acmicpc.net/problem/1157

--- src/baekjoon/BOJ_1157.java
+++ src/baekjoon/BOJ_1157.java
@@ -3,6 +3,8 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 // 단어 공부
 // https://www.acmicpc.net/problem/1157
@@ -11,8 +13,23 @@ public class BOJ_1157 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] st = br.readLine().split("");
 
+        Map<String, Integer> map = new HashMap<>();
+
         for (String s : st) {
-            System.out.println(s);
+            map.put(s.toUpperCase(), map.getOrDefault(s.toUpperCase(), 0) + 1);
         }
+
+        int max = 0;
+        String maxKey = "";
+
+        for (String key : map.keySet()) {
+            if (max < map.get(key)) {
+                max = map.get(key);
+                maxKey = key;
+            } else if (max == map.get(key)) {
+                maxKey = "?";
+            }
+        }
+        System.out.println(maxKey);
     }
 }
