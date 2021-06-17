d94ba0ee621aec3fb6c1b99e5fdf9ef431c7d63c
jaesungahn91 <jaesungahn91@gmail.com>
2021-06-17 09:00:10 +0900

    [BOJ, 2475] 검증수 https://www.acmicpc.net/problem/2475

---
+++ src/baekjoon/BOJ_2475.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 검증수
+// https://www.acmicpc.net/problem/2475
+public class BOJ_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int sum = 0;
+
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += (num*num);
+        }
+
+        System.out.println(sum % 10);
+    }
+}
