70537f50586ad3c969583c001e4e3215b29f32d3
jaesungahn91 <jaesungahn91@gmail.com>
2021-07-14 14:37:07 +0900

    [BOJ, 2475] 검증수 https://www.acmicpc.net/problem/2475

---
+++ src/baekjoon/BOJ_2562.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 최댓값
+public class BOJ_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int maxIdx = 0;
+        for (int i=0; i<9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (max < num) {
+                max = num;
+                maxIdx = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(maxIdx);
+    }
+}
