50c67d453c316cb8994475dcbd5ce325eb4569be
jaesungahn91 <jaesungahn91@gmail.com>
2021-07-22 08:48:57 +0900

    [BOJ, 10171] 고양이 https://www.acmicpc.net/problem/10171

---
+++ src/baekjoon/class_1/BOJ_10171.java
@@ -0,0 +1,11 @@
+package baekjoon.class_1;
+
+// 고양이
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
