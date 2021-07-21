d0d1432c96c189851cc8e2ed48f178d3bbd86264
jaesungahn91 <jaesungahn91@gmail.com>
2021-07-21 10:49:39 +0900

    [BOJ, 11654] 아스키코드 https://www.acmicpc.net/problem/11654

---
+++ src/baekjoon/class_1/BOJ_11654.java
@@ -0,0 +1,14 @@
+package baekjoon.class_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 아스키 코드
+public class BOJ_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int ch = br.readLine().charAt(0);
+        System.out.println(ch);
+    }
+}
