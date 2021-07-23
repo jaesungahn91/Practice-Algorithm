633557c0cdf2561252e6b980a0629045702dd7ff
jaesungahn91 <jaesungahn91@gmail.com>
2021-07-23 20:38:16 +0900

    [BOJ, 10809] 알파벳 찾기
    https://www.acmicpc.net/problem/10809

--- src/baekjoon/class_1/BOJ_10809.java
+++ src/baekjoon/class_1/BOJ_10809.java
@@ -1,8 +1,33 @@
 package baekjoon.class_1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 알파벳 찾기
 public class BOJ_10809 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
 
+        char[] alphabetArr = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] chrArr = S.toCharArray();
+
+        for (int i=0; i<alphabetArr.length; i++) {
+            boolean chk = false;
+            int idx = 0;
+            for (int j=0; j<chrArr.length; j++) {
+                if (alphabetArr[i] == chrArr[j]) {
+                    chk = true;
+                    idx = j;
+                    break;
+                }
+            }
+            if (chk) {
+                System.out.print(idx + " ");
+            } else {
+                System.out.print("-1 ");
+            }
+        }
     }
 }
