821dd8a44c91dacb6c6b2f9a563e84e7eec7fdec
jaesungahn91 <jaesungahn91@gmail.com>
2021-07-24 23:11:50 +0900

    [BOJ, 1018] 체스판 다시 칠하기
    https://www.acmicpc.net/problem/1018

---
+++ src/baekjoon/class_2/BOJ_1018.java
@@ -0,0 +1,21 @@
+package baekjoon.class_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 체스판 다시 칠하기
+public class BOJ_1018 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String NM = br.readLine();
+        int N = Integer.parseInt(NM.split(" ")[0]);
+        int M = Integer.parseInt(NM.split(" ")[1]);
+
+//        for (int i = 0; i < N; i++) {
+//            for () {
+//
+//            }
+//        }
+    }
+}
