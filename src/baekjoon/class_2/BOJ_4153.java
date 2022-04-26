f08a451569128af84a6bae5dfb3af418b7208928
jaesungahn91 <jaesungahn91@gmail.com>
2022-04-26 16:37:02 +0900

    [BOJ, 4153] 직각삼각형
    https://www.acmicpc.net/problem/4153

---
+++ src/baekjoon/class_2/BOJ_4153.java
@@ -0,0 +1,35 @@
+package baekjoon.class_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_4153 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            int[] arr = new int[3];
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+
+            if(arr[0] == 0 && arr[1] == 0 && arr[2] ==0) break;
+
+            Arrays.sort(arr);
+            if(Math.pow(arr[2], 2) == Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+
+        }
+
+    }
+
+}
