0ac4760c130fd9d0a2913df1db615637fdc6e473
안재성 <jaesungahn91@gmail.com>
2022-08-16 12:40:04 +0900

    Time: 2 ms (88.60%), Space: 48.2 MB (81.64%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -5,14 +5,20 @@ class Solution {
         int[][] map = new int[mat.length][2];
 
         for (int i = 0; i < mat.length; i++) {
-            int soldier = 0;
-            for (int j = 0; j < mat[i].length; j++) {
-                if(mat[i][j] == 1) {
-                    soldier++;
+            int cnt = 0;
+            int start = 0;
+            int end = mat[0].length-1;
+            while(start <= end){
+                int mid = start + (end-start)/2;
+                if(mat[i][mid] == 1){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
                 }
             }
             map[i][0] = i;
-            map[i][1] = soldier;
+            map[i][1] = start;
+            cnt = 0;
         }
 
         Arrays.sort(map,(o1, o2) -> o1[1]!=o2[1]? o1[1]-o2[1] : o1[0]-o2[0]);
@@ -22,5 +28,4 @@ class Solution {
         }
         return result;
     }
-    
 }
