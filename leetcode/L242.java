7dd425fc7b3593098634793129f4d2bdd701e24a
jaesungahn91 <jaesungahn91@gmail.com>
2023-07-10 23:27:20 +0900

    [leetcode, 242] Valid Anagram
    https://leetcode.com/problems/valid-anagram/

--- leetcode/L242.java
+++ leetcode/L242.java
@@ -1,8 +1,37 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // 242. Valid Anagram
 public class L242 {
 
+    public static void main(String[] args) {
+        String s = "aacc";
+        String t = "ccac";
+        System.out.println(isAnagram(s, t));
+    }
 
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> maps = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            maps.put(c, maps.getOrDefault(c, 0) + 1);
+        }
+        Map<Character, Integer> mapt = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            mapt.put(c, mapt.getOrDefault(c, 0) + 1);
+        }
+
+        if (maps.equals(mapt)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
--- 백준/Silver/9012. 괄호/B9012.java
+++ 백준/Silver/9012. 괄호/B9012.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
+class B9012 {
 
 	public static void main(String[] args) throws Exception {
 
