63dc974b4f35d01048e4e9357f6f47e35382ce06
jaesungahn91 <jaesungahn91@gmail.com>
2023-07-18 23:51:32 +0900

    [leetcode, 88] Merge Sorted Array
    https://leetcode.com/problems/merge-sorted-array/

--- leetcode/L88.java
+++ leetcode/L88.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class L88 {
 
     public static void main(String[] args) {
-
+        System.out.println();
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
