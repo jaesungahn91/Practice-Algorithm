283305c13eb4d0ab42b346f2de3ab66876120cbf
jaesungahn91 <jaesungahn91@gmail.com>
2023-08-03 07:24:37 +0900

    [leetcode, 859] Buddy Strings
    https://leetcode.com/problems/buddy-strings/

--- leetcode/L859.java
+++ leetcode/L859.java
@@ -21,7 +21,7 @@ public class L859 {
             return set.size() < s.length();
         } else {
             List<Integer> list = new ArrayList<>();
-            for (int i = 0;i<s.length();i++)
+            for (int i = 0; i < s.length(); i++)
                 if (s.charAt(i) != goal.charAt(i)) list.add(i);
             return list.size() == 2 && s.charAt(list.get(0)) == goal.charAt(list.get(1)) && goal.charAt(list.get(0)) == s.charAt(list.get(1));
         }
