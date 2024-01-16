6ceb1ce39c2b666b777d240d721c43caf7dbe5d3
jaesungahn91 <jaesungahn91@gmail.com>
2024-01-17 08:39:14 +0900

    [leetcode, 27] Remove Element
    https://leetcode.com/problems/remove-element/

---
+++ leetcode/L27.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 27. Remove Element
+public class L27 {
+
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        return i;
+    }
+
+}
